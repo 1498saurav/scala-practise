d6b002f908fc39626a8a06c6fa69e55de5d6fd3f
saurav.sawant <1498saurav@gmail.com>
2024-04-19 12:36:44 +0530

    +semver: skip added new files

--- src/main/scala/practise/consec.scala
+++ src/main/scala/practise/consec.scala
@@ -1,17 +1,32 @@
 package practise
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.functions.{col, row_number}
 
 object consec extends App{
 
   val spark: SparkSession =SparkSession.builder().appName("consec").master("local[*]").getOrCreate()
 
   val options:Map[String,String]=Map(
-    "headers"->"true",
-    "inferSchema"->"true"
+    "header"->"true",
+    "inferSchema"->"true",
+    "delimiter"->"|"
   )
 
-  private val df=spark.read.format("csv").options(options).load("src/main/resources/consec.csv")
-  df.show(false)
+  spark.sparkContext.setLogLevel("error")
+
+  private val df=spark.read.format("csv").options(options).load("src/main/resources/consec.csv").withColumn("num",col("num").cast("int"))
+
+  val window=Window.partitionBy("num").orderBy(col("id"))
+
+    df.withColumn("row_number",row_number().over(window))
+      .withColumn("difference",col("id")-col("row_number"))
+      .groupBy("num","difference").count()
+      .filter("count>2")
+      .select("num","count")
+      .show(false)
+
+
 
 }
